583d6c645136537c109b1a3509bd6c31f1b1005c
Nick Deal <dealnick@gmail.com>
2019-10-08 16:22:56 -0500

    basic flask server

--- app.py
+++ app.py
@@ -1,14 +1,15 @@
 from flask import Flask
+from flask import render_template
 
 app = Flask(__name__)
 
 @app.route('/')
 def index():
-    return "hello world"
+    return render_template('index.html', pageTitle='My Calculator')
 
 @app.route('/nick')
 def nick():
-    return "hello Nick!"
+    return render_template('nick.html', pageTitle='About Nick')
 
     if __name__ == '__main__':
         app.run(debug=True)

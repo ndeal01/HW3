d8d1e95ddf2c40aa92550dbbcba0e475e07a0826
Nick Deal <dealnick@gmail.com>
2019-10-08 18:14:49 -0500

    Added loan amount, number of payments, interest rate, and discount factor into index.html

--- app.py
+++ app.py
@@ -5,11 +5,8 @@ app = Flask(__name__)
 
 @app.route('/')
 def index():
-    return render_template('index.html', pageTitle='My Calculator')
+    return render_template('index.html', pageTitle='Loan Calculator')
 
-@app.route('/nick')
-def nick():
-    return render_template('nick.html', pageTitle='About Nick')
 
     if __name__ == '__main__':
         app.run(debug=True)

de1d0e21f97d6edc1008e85ebed73a8991bfc8cd
Nick Deal <dealnick@gmail.com>
2019-10-08 19:21:04 -0500

    finished form by adding different variables; started app.py; updated base.html

--- app.py
+++ app.py
@@ -7,6 +7,21 @@ app = Flask(__name__)
 def index():
     return render_template('index.html', pageTitle='Loan Calculator')
 
+@app.route('/payment', methods=['GET', 'POST'])
+def calculation():
+    if request.method == 'POST':
+        form = request.form
+        A = int(form['A'])
+        p = int(form['p'])
+        y = int(form['y'])
+        r = int(form['r'])
+        f = int(form['f'])
+        Discount = (((1+(r/f))^(p*y))-1)/((r/f)(1+(r/f)^(p*y)))
+        calc = A/Discount
+        return render_template('index.html', display=calc, pageTitle='Loan Calculator')
+
+    return redirect("/")
+
 
     if __name__ == '__main__':
         app.run(debug=True)
